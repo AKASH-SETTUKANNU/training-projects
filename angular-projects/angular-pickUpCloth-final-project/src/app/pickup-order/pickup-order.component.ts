e69060b045d9d64eeafc4a65f6712cfed48f9892
Akash Settukannu <akash.settukannu@claysys.com>
2024-09-19 10:18:19 +0530

    update code

---
+++ angular-projects/angular-pickUpCloth-final-project/src/app/pickup-order/pickup-order.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pickup-order',
+  standalone: true,
+  imports: [],
+  templateUrl: './pickup-order.component.html',
+  styleUrl: './pickup-order.component.css'
+})
+export class PickupOrderComponent {
+
+}
