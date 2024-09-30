3916901a52ab2ec08991be8a15ce9bd152fdb9c2
akash-settukannu <akash.settukannu@claysys.net>
2024-09-30 13:20:14 +0530

    update code

--- angular-projects/angular-event-management-pro/src/app/header/header.component.ts
+++ angular-projects/angular-event-management-pro/src/app/header/header.component.ts
@@ -1,34 +1,54 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { StorageService } from '../storage/storage.service';
+import { Invitation, User } from '../models/user';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   showEventAddBlock = false;
   showNotificationDisplay = false;
   showProfileEditArea = false;
-  searchText:string='';
+  searchText: string = '';
+
+  isShowNotificationInfo: boolean = false;
+  notifications: Invitation[] = [];
+  detailNotification: Invitation | undefined;
 
   profile: { name: string; dateOfBirth: string };
 
-  constructor() {
+  constructor(private storageService: StorageService) {
     const currentUserDetails = JSON.parse(localStorage.getItem('loggedInUser') || 'null');
-    
     this.profile = {
       name: currentUserDetails ? currentUserDetails.userName : 'Guest',
-      dateOfBirth:currentUserDetails ? currentUserDetails.userBirthDate:'unknown'
+      dateOfBirth: currentUserDetails ? currentUserDetails.userBirthDate : 'unknown'
     };
   }
 
-  notifications = [
-    { name: 'Koushikk', message: 'Inviting for his marriage ajadhjadsftfwefwe7 wdo d' },
-  ];
+  ngOnInit(): void {
+    console.log('Component initialized');
+    this.loadNotifications();
+  }
+
+  loadNotifications(): void {
+    const loggedUser = this.storageService.getLoggedInUser();
+    const users = this.storageService.getUsers();
+    
+    const user = users.find((u: User) => u.userEmail === loggedUser?.userEmail);
+    
+    if (user) {
+      this.notifications = user.notifications || [];
+      console.log("Notifications loaded:", this.notifications);
+    } else {
+      console.error("User not found");
+    }
+  }
 
   eventCategories = [
     { name: 'Birthday', icon: 'fa-solid fa-cake-candles' },
@@ -37,8 +57,7 @@ export class HeaderComponent {
     { name: 'Others', icon: 'fa-solid fa-gift' },
   ];
 
-  menubarDisplay() {
-  }
+  menubarDisplay() {}
 
   addEvent() {
     this.showEventAddBlock = !this.showEventAddBlock;
@@ -50,5 +69,23 @@ export class HeaderComponent {
 
   displayLogout() {
     this.showProfileEditArea = !this.showProfileEditArea;
+  }
+
+  showNotificationInfo(notificationId: number) {
+    this.isShowNotificationInfo = true;
+    const loggedUser = this.storageService.getLoggedInUser();
+    const users = this.storageService.getUsers();
+    
+    const user = users.find((u: User) => u.userEmail === loggedUser?.userEmail);
+    
+    if (user) {
+      this.detailNotification = user.notifications?.find((n: Invitation) => n.eventId === notificationId);
+    } else {
+      console.error("User not found");
+    }
+  }
+
+  closenotification(): void {
+    this.isShowNotificationInfo = false;
   }
 }
